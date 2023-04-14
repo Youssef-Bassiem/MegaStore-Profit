17b01b5c94f68010b4c5fe644a652fa0d41683c7
killerx201 <111208582+killerx201@users.noreply.github.com>
2023-04-15 01:56:57 +0200

    update_models

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 import datetime
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics
+
 
 def DateFormat(mega_store):
     mega_store['Order Date'] = pd.to_datetime(mega_store['Order Date'], dayfirst=True)
@@ -71,3 +74,96 @@ Y_Data = mega_store['Profit']
 X_Data = mega_store.drop(['Profit'] ,axis = 1)
 
 print(X_Data.shape)
+print(corr)
+
+
+
+# #lasso reg
+# from sklearn.linear_model import LassoCV
+# from sklearn.model_selection import RepeatedKFold
+#
+# X = mega_store[["Sales","Ship Day"]]
+# y = mega_store["Profit"]
+#
+# #define cross-validation method to evaluate model
+# #cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+# cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=20)
+# #define model
+# clf = LassoCV(alphas = np.logspace(-1,1,3), cv=cv, max_iter = 1000000, tol = 0.005)
+# #model = LassoCV(alphas=np.arange(0, 1, 0.01), cv=cv)
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.2, random_state=0)
+# #fit model
+# clf.fit(X_train, y_train)
+#
+# print("Mean Square Error lasso", metrics.mean_squared_error(y_test,  clf.predict(X_test)))
+
+
+#predict hp value using lasso regression model
+# print(clf.predict(X_test))
+
+
+#polynomial
+
+# def Polynomial(x ,n ,column ,data ,index=0 ,powers=0):
+#     if index == x.shape[1]:
+#         if powers <= n:
+#             data = np.append(data ,column ,axis=1)
+#         return data
+#
+#     for i in range(n+1):
+#         col = np.power(x[:,index] ,i).reshape(-1,1)
+#         mult = col * column
+#         data = Polynomial(x ,n ,mult ,data ,index+1 ,powers+i)
+#     return data
+#
+# from sklearn import linear_model
+# X = mega_store[["Sales","Ship Day"]]
+# y = mega_store["Profit"]
+#
+# x_data = Polynomial(X ,2 ,column= np.ones((X.shape[0] ,1)) ,data= np.zeros((X.shape[0] ,1)))
+#
+# model= linear_model.LinearRegression()
+#
+# X_train, X_test, y_train, y_test = train_test_split(
+#     x_data, y, test_size=0.2, random_state=0)
+# #fit model
+# model.fit(X_train, y_train)
+#
+# print("Mean Square Error", metrics.mean_squared_error(y_test,  model.predict(X_test)))
+#
+# #predict hp value using lasso regression model
+
+
+from sklearn.linear_model import LinearRegression
+#polynomial
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+X=mega_store[["Sales","MainCategory"]]
+y=mega_store["Profit"]
+
+model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression(fit_intercept = False))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+#fit model
+model.fit(X_train, y_train)
+
+print("Mean Square Error poly", metrics.mean_squared_error(y_test,  model.predict(X_test)))
+
+# print(model.namedsteps.linearregression.coef)
+# print(model.predict(X_predict))
+
+#ridge
+from sklearn.linear_model import Ridge
+X=mega_store[["Ship Day","Ship Month","Ship Year"]]
+y=mega_store["Profit"]
+# X = mega_store.drop(['Profit'] ,axis = 1)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=10,shuffle=True)
+
+model = Ridge(alpha=1.0)
+model.fit(X_train, y_train)
+
+print("Mean Square Error ridge", metrics.mean_squared_error(y_test,  model.predict(X_test)))
